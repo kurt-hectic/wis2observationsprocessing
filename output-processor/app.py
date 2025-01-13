ff1d55ab2afcb8391f95c5e0f90572f46c8bb658
Timo Proescholdt <tproescholdt@wmo.int>
2025-01-13 08:47:29 +0000

    check if keys are configured and present in message

--- output-processor/app.py
+++ output-processor/app.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pyjq
+import uuid
 
 from abc import ABC, abstractmethod
 
@@ -18,16 +19,6 @@ logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=log_lev
 NR_RECORDS_COMMITTED = Counter('records_committed_total', 'Number of records committed to the database')
 NR_RECORDS_NOT_PARSED = Counter('records_not_parsed_total', 'Number of records not parsed')
 NR_RECORD_EMPTY_VALUES = Counter('records_empty_values_total', 'Number of records with empty values')
-
-def format_iso(*args):
-        datestr = args[0]
-        if datestr is None or datestr == "":
-            return None
-        try:
-            return isoparser.isoparse(datestr).replace(microsecond=0).isoformat()
-        except Exception as e:
-            logging.warning(f"error formatting date {datestr}. Error: {e}")
-            return None
 
 
 
@@ -54,7 +45,11 @@ class OutputProcessor(BaseProcessor):
                 d = self.pattern.first(observation)
                 values.append(d)
 
-                key = "-".join([str(d[k]) for k in self.keys])
+                if not all( [k in d for k in self.keys] ):
+                    logging.warning("keys %s not found in record %s",self.keys, d)
+                    key = uuid.uuid4()
+                else:
+                    key = "-".join([str(d[k]) for k in self.keys])
                 keys.append(key)
                 
                 if any( [v is None for v in d.values()] ):
@@ -75,6 +70,8 @@ if __name__ == "__main__":
     pattern = open( os.getenv("DATA_MAPPING_FILE"),encoding="utf8",mode="r").read()  
     group_id = os.getenv("GROUP_ID")
     keys = os.getenv("KEYS").split(",")
+    if len(keys) == 0:
+        raise ValueError("keys must be defined")
    
     processor = OutputProcessor(pattern=pattern,group_id=group_id,keys=keys)
     processor.start_consuming()
